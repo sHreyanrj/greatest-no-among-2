db86669c96b6c7520d16b2f0a8bea56a6ec829eb
sHrEy_anrJ <95269614+sHreyanrj@users.noreply.github.com>
2022-04-26 21:27:40 +0530

    Update and rename README.md to home.py

---
+++ home.py
@@ -0,0 +1,12 @@
+x=int(input("Enter the number: "))
+y=int(input("Enter the number: "))
+z=int(input("Enter teh number: "))
+print(x)
+print(y)
+print(z)
+if x > y and x > z:
+   print("x is greatest")
+elif y > x and y > z: 
+   print("y is greatest")
+else:
+    print("z is greatest")
